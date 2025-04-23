8d8628abdf790707897f221bc057b1fc36f3b2fd
LEAKONO <leakonoemmanuel3@gmail.com>
2025-04-23 20:02:43 +0300

    feat: add About page component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 function About() {
   return (
     <div>
-      
+      <h1>Leon</h1>
+      <p> hi my name is leon leakono im a boy turning 10 and i 
+        like to learn new things like progtaming when i see some one talking  
+        about and my sebling are kailin my big sister she is turning 13this year 
+        andy big brother wh is called leslie leakono and his turning 16 and his sometimes his 
+        a bad person and mom is called felister leakono and she is turning 37 and my dad is 
+        called sanico leakono and i have  big cosin called emnwel leakono and h
+        is turnig 21 an aunt that is turning 22 and shes called joyce </p>
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      
+      <h3>Welcome to my website</h3>
     </div>
   )
 }
