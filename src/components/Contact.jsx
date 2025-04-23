55b44b83a3f48798d351286447e767f6a8fed564
LEAKONO <leakonoemmanuel3@gmail.com>
2025-04-23 20:07:16 +0300

    feat: add About page component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 function Contact() {
   return (
     <div>
+      <h2>Contact US</h2>
+      <p>You can reach out via email or phone anytime.</p>
       
     </div>
   )
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 function Services() {
   return (
     <div>
-        
+        <ul>
+            <li>Web Design</li>
+            <li>Web Development</li>
+            <li>Api Design</li>
+        </ul>
       
     </div>
   )
